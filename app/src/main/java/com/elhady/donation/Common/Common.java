9b4f1106f5ba5dc74b5abddb7ac137f3ea47f733
islamelhady <i.elhady44@gmail.com>
2019-12-17 08:43:19 +0200

    common and interfac

---
+++ app/src/main/java/com/elhady/donation/Common/Common.java
@@ -0,0 +1,10 @@
+package com.elhady.donation.Common;
+
+import com.elhady.donationp.Model.User;
+
+/**
+ * Created by El-hady on 4/21/2018.
+ */
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/elhady/donation/interface/ItemClickListener.java
@@ -0,0 +1,10 @@
+package com.elhady.donationp.Interface;
+
+import android.view.View;
+
+/**
+ * Created by El-hady on 4/21/2018.
+ */
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
